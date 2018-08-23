0df225b19669451eee7be573b6ad7bea99b4d535
Javi Carrion <suavejavi@google.com>
2018-08-23 17:31:14 -0400

    feat: Add array of kitty objects

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,18 @@
 
-let image = document.querySelector('#kitty-image'),
-    counter = 0;
+const kitties = [
+    {
+        name: 'Reveal',
+        image: '../assets/img/animal-3118722_640.jpg',
+        counter: 0
+    },
+    {
+        name: 'Comfy',
+        image: '../assets/img/cat-197175_640.jpg',
+        counter: 0
+    },
+]
+
+let image = document.querySelector('#kitty-image');
 
 image.addEventListener('click', () => {
     let kittyCount = document.querySelector('.count')
