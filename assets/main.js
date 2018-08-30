85c9f7ec3cb1dd182dc935a15e1fff210a2e9b1a
Javi Carrion <suavejavi@google.com>
2018-08-30 17:31:46 -0400

    feat: Add more kitties to the kitties array

--- assets/main.js
+++ assets/main.js
@@ -10,6 +10,72 @@ const kitties = [
     image: 'assets/img/cat-197175_640.jpg',
     counter: 0,
     id: 2
+  },
+  {
+    name: 'Under',
+    image: 'assets/img/cat-278191_640.jpg',
+    counter: 0,
+    id: 3
+  },
+  {
+    name: 'Glasses',
+    image: 'assets/img/cat-984097_640.jpg',
+    counter: 0,
+    id: 4
+  },
+  {
+    name: 'Wink',
+    image: 'assets/img/cat-1333926_640.jpg',
+    counter: 0,
+    id: 5
+  },
+  {
+    name: 'Lean',
+    image: 'assets/img/cat-2151382_640.jpg',
+    counter: 0,
+    id: 6
+  },
+  {
+    name: 'See',
+    image: 'assets/img/cat-2258190_640.jpg',
+    counter: 0,
+    id: 7
+  },
+  {
+    name: 'Side',
+    image: 'assets/img/cat-2348997_640.jpg',
+    counter: 0,
+    id: 8
+  },
+  {
+    name: 'Cover',
+    image: 'assets/img/cat-2806957_640.jpg',
+    counter: 0,
+    id: 9
+  },
+  {
+    name: 'Wide',
+    image: 'assets/img/cat-3553106_640.jpg',
+    counter: 0,
+    id: 10
+  },
+  {
+    name: 'Bowl',
+    image: 'assets/img/cats-3606836_640.jpg',
+    counter: 0,
+    id: 11
+  },
+  {
+    name: 'Mean',
+    image: 'assets/img/doll-1636212_640.jpg',
+    counter: 0,
+    id: 12
+  },
+  {
+    name: 'Plant',
+    image: 'assets/img/kitten-looking-at-you-1995976_640.jpg',
+    counter: 0,
+    id: 13
   }
 ];
 
