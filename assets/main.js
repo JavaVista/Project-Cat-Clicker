62f2778950aae916ebe9e349b3c8df6dbd803038
Javi Carrion <suavejavi@google.com>
2018-09-05 18:06:09 -0400

    refactor: HTML plus JS for list of cats using MVO

--- assets/main.js
+++ assets/main.js
@@ -4,78 +4,91 @@ const model = [
     name: 'Reveal',
     image: 'assets/img/animal-3118722_640.jpg',
     counter: 0,
+    selected: false,
     id: 1
   },
   {
     name: 'Comfy',
     image: 'assets/img/cat-197175_640.jpg',
     counter: 0,
+    selected: false,
     id: 2
   },
   {
     name: 'Under',
     image: 'assets/img/cat-278191_640.jpg',
     counter: 0,
+    selected: false,
     id: 3
   },
   {
     name: 'Glasses',
     image: 'assets/img/cat-984097_640.jpg',
     counter: 0,
+    selected: false,
     id: 4
   },
   {
     name: 'Wink',
     image: 'assets/img/cat-1333926_640.jpg',
     counter: 0,
+    selected: false,
     id: 5
   },
   {
     name: 'Lean',
     image: 'assets/img/cat-2151382_640.jpg',
     counter: 0,
+    selected: false,
     id: 6
   },
   {
     name: 'See',
     image: 'assets/img/cat-2258190_640.jpg',
     counter: 0,
+    selected: false,
     id: 7
   },
   {
     name: 'Side',
     image: 'assets/img/cat-2348997_640.jpg',
     counter: 0,
+    selected: false,
     id: 8
   },
   {
     name: 'Cover',
     image: 'assets/img/cat-2806957_640.jpg',
     counter: 0,
+    selected: false,
     id: 9
   },
   {
     name: 'Wide',
     image: 'assets/img/cat-3553106_640.jpg',
     counter: 0,
+    selected: false,
     id: 10
   },
   {
     name: 'Bowl',
     image: 'assets/img/cats-3606836_640.jpg',
     counter: 0,
+    selected: false,
     id: 11
   },
   {
     name: 'Mean',
     image: 'assets/img/doll-1636212_640.jpg',
     counter: 0,
+    selected: false,
     id: 12
   },
   {
     name: 'Plant',
     image: 'assets/img/kitten-looking-at-you-1995976_640.jpg',
     counter: 0,
+    selected: false,
     id: 13
   }
 ];
@@ -86,6 +99,7 @@ const octopus = (() => {
   function init() {
     kittyView.handler();
     kittyView.render(selectedKitty);
+    kittyList.render(model);
   }
   function incrementCat(e) {
     selectedKitty.counter++;
@@ -103,9 +117,9 @@ window.addEventListener('load', octopus.init);
 // View
 const kittyView = (() => {
   let element = document.querySelector('.content');
+  //let kittyImage = document.querySelector('.kitty-image');
 
   function handler() {
-    //let kittyImage = document.querySelector('.kitty-image');
     element.addEventListener('click' , octopus.incrementCat, false);
   }
 
@@ -118,10 +132,23 @@ const kittyView = (() => {
     </div>`;
   }
   return {
-    handler,
-    render
+    render,
+    handler
   };
 })();
+
+const kittyList = ((listElement) => {
+  const kittyDisplay = listElement;
+  function render(cats) {
+    kittyDisplay.innerHTML = `<h2>List of Kitties</h2>`;
+    cats.forEach(cat => {
+      kittyDisplay.innerHTML += `<li><button id=${cat.id}>${cat.name}</button></li>`;
+    });
+ }
+  return {
+    render
+}
+})(document.querySelector('.side'));
 
 
 
